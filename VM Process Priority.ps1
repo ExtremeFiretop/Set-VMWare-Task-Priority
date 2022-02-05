e0cb6ce20044adf59482c93da9e46011e2fed22a
Joel Samson <joel.samson@hotmail.com>
2022-02-05 17:55:12 -0500

    Add files via upload

---
+++ VM Process Priority.ps1
@@ -0,0 +1,5 @@
+﻿Get-WmiObject Win32_process -filter 'name = "vmware-vmx.exe"' | foreach-object { $_.SetPriority(128) }
+Get-WmiObject Win32_process -filter 'name = "vmware-hostd.exe"' | foreach-object { $_.SetPriority(128) }
+Get-WmiObject Win32_process -filter 'name = "vmware-authd.exe"' | foreach-object { $_.SetPriority(128) }
+Get-WmiObject Win32_process -filter 'name = "vmwaredhcp.exe"' | foreach-object { $_.SetPriority(128) }
+Get-WmiObject Win32_process -filter 'name = "vmwarenat.exe"' | foreach-object { $_.SetPriority(128) }
